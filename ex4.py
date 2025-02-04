1a20beab353dd85d53b484e0e0386b817ff33423
Víctor Donato <joao.ddonato@gmail.com>
2025-02-04 19:24:13 -0300

    Versão nova da calculadora de soma

--- ex4.py
+++ ex4.py
@@ -1,4 +1,5 @@
 n1 = float(input('Qual o primeiro número? '))
 n2 = float(input('Qual o segundo número? '))
-
-print('A adição é:', n1 + n2)
+s  = n1 + n2
+# print('A adição entre:', n1, 'e', n2, 'vale' s -> fomarto antigo do Python
+print('a adição entre {} e {} vale {}'.format(n1,n2,s))
---
+++ exp.py
@@ -0,0 +1,4 @@
+n =input('Digite Algo')
+print (n.isnumeric())
+#existe também isalpha para saber se é alpha ou seja alfabético
+#isalnum para saber se é alfanumérico
